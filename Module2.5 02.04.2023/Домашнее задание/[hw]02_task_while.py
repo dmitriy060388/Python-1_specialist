0d9bca3777b8c34c07777970cfea54ddc2a3426d
dsokolov <dsokolov@aladdin.ru>
2023-04-06 07:29:05 +0300

    решил задачу [hw]02_task_while модуля 2.5 домашнего задания

--- Module2.5 02.04.2023/Домашнее задание/[hw]02_task_while.py
+++ Module2.5 02.04.2023/Домашнее задание/[hw]02_task_while.py
@@ -13,3 +13,11 @@
 
 a = int(input("a: "))
 b = int(input("b: "))
+
+for i in range(a, b+1):
+    if i % 5 == 0:
+        print(i)
+if a > b:
+    for i in range(b, a+1):
+        if i % 5 == 0:
+            print(i)
